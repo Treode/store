37af39a39abb06059467f64cd8c87bef5d5c7a31
Topher <topher@treode.com>
2013-10-16 09:48:45 -0700

    Avoid reflective calls for performance.

--- cluster/src/com/treode/cluster/messenger/package.scala
+++ cluster/src/com/treode/cluster/messenger/package.scala
@@ -1,7 +1,5 @@
 package com.treode.cluster
 
-import scala.language.reflectiveCalls
-
 import com.esotericsoftware.kryo.io.{Input, Output}
 import com.treode.cluster.concurrent.Callback
 import com.treode.cluster.io.Socket
@@ -9,62 +7,60 @@ import com.treode.cluster.misc.KryoPool
 
 package object messenger {
 
-  private [messenger] def fill (socket: Socket, input: Input, length: Int, cb: Callback [Unit]) {
+  private class Filler (socket: Socket, input: Input, length: Int, cb: Callback [Unit])
+  extends Callback [Int] {
 
-    if (length <= input.limit - input.position) {
-      cb()
+    private [this] val buffer = KryoPool.wrap (input)
 
-    } else {
-
-      KryoPool.ensure (input, length)
-
-      val loop = new Callback [Int] {
-
-        val buffer = KryoPool.wrap (input)
-
-        def ensure() {
-          if (length <= buffer.position - input.position)
-            cb()
-          else
-            socket.read (buffer, this)
-        }
-
-        def apply (result: Int) {
-          if (result < 0) {
-            socket.close()
-          } else {
-            input.setLimit (input.limit + result)
-            ensure()
-          }}
-
-        def fail (t: Throwable) = cb.fail (t)
-      }
-
-      loop.ensure()
-    }}
-
-  private [messenger] def flush (socket: Socket, output: Output, cb: Callback [Unit]) {
-
-    val loop = new Callback [Int] {
-
-      val buffer = KryoPool.wrap (output)
-
-      def flush() {
-        if (buffer.remaining == 0) {
-          cb()
-        } else {
-          socket.write (buffer, this)
-        }}
-
-      def apply (result: Int) {
-        if (result < 0)
-          socket.close()
-        else
-          flush()
-      }
-
-      def fail (t: Throwable) = cb.fail (t)
+    def fill() {
+      if (length <= buffer.position - input.position)
+        cb()
+      else
+        socket.read (buffer, this)
     }
 
-    loop.flush()
-  }}
+    def apply (result: Int) {
+      if (result < 0) {
+        socket.close()
+      } else {
+        input.setLimit (input.limit + result)
+        fill()
+      }}
+
+    def fail (t: Throwable) = cb.fail (t)
+  }
+
+  private [messenger] def fill (socket: Socket, input: Input, length: Int, cb: Callback [Unit]) {
+    if (length <= input.limit - input.position) {
+      cb()
+    } else {
+      KryoPool.ensure (input, length)
+      new Filler (socket, input, length, cb) .fill()
+    }}
+
+  private class Flusher (socket: Socket, output: Output, cb: Callback [Unit])
+  extends Callback [Int] {
+
+    private [this] val buffer = KryoPool.wrap (output)
+
+    def flush() {
+      if (buffer.remaining == 0) {
+        output.setPosition (0)
+        cb()
+      } else {
+        socket.write (buffer, this)
+      }}
+
+    def apply (result: Int) {
+      if (result < 0)
+        socket.close()
+      else
+        flush()
+    }
+
+    def fail (t: Throwable) = cb.fail (t)
+  }
+
+  private [messenger] def flush (socket: Socket, output: Output, cb: Callback [Unit]): Unit =
+    new Flusher (socket, output, cb) .flush()
+}
