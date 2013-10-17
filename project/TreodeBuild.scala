a0cc3babbf96f60679eb7a866dc231993bcf8f79
Topher <topher@treode.com>
2013-10-17 10:33:55 -0700

    Create stub configurations in build.

--- project/TreodeBuild.scala
+++ project/TreodeBuild.scala
@@ -1,21 +1,33 @@
 import sbt._
 import Keys._
 
+import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
 import sbtassembly.Plugin.AssemblyKeys._
 import sbtassembly.Plugin.assemblySettings
 
 object TreodeBuild extends Build {
 
-  lazy val standardSettings = Seq (
+  // Adds stub configuration.
+  lazy val Stub = config ("stub") extend (Compile)
+  lazy val CustomTest = config ("test") extend (Stub)
+
+  lazy val commonSettings =
+    inConfig (CustomTest) (Defaults.testSettings) ++
+    Seq (
 
     organization := "com.treode",
     version := "0.1",
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.10.3",
+
+    ivyConfigurations := overrideConfigs (Compile, Stub, CustomTest) (ivyConfigurations.value),
 
     unmanagedSourceDirectories in Compile <<=
       (baseDirectory ((base: File) => Seq (base / "src"))),
 
-    unmanagedSourceDirectories in Test <<=
+    unmanagedSourceDirectories in Stub <<=
+      (baseDirectory ((base: File) => Seq (base / "stub"))),
+
+    unmanagedSourceDirectories in CustomTest <<=
       (baseDirectory ((base: File) => Seq (base / "test"))),
 
     scalacOptions ++= Seq ("-deprecation", "-feature", "-optimize", "-unchecked"),
@@ -28,13 +40,30 @@ object TreodeBuild extends Build {
       "com.google.guava" % "guava" % "14.0.1",
       "com.yammer.metrics" % "metrics-core" % "3.0.0-BETA1",
       "org.slf4j" % "slf4j-api" % "1.7.2",
-      "org.slf4j" % "slf4j-simple" % "1.7.2"),
+      "org.slf4j" % "slf4j-simple" % "1.7.2"))
+
+  lazy val standardSettings =
+    commonSettings ++
+    Seq (
 
     libraryDependencies ++= Seq (
       "com.github.axel22" %% "scalameter" % "0.3" % "test",
       "org.scalamock" %% "scalamock-scalatest-support" % "3.0.1" % "test",
       "org.scalatest" %% "scalatest" % "2.0.M5b" % "test",
       "org.scalacheck" %% "scalacheck" % "1.10.1" % "test"))
+
+  lazy val withStubSettings =
+    inConfig (Stub) (Defaults.configSettings) ++
+    commonSettings ++
+    Seq (
+
+    EclipseKeys.configurations := Set (Compile, Stub, CustomTest),
+
+    libraryDependencies ++= Seq (
+      "com.github.axel22" %% "scalameter" % "0.3" % "stub->default",
+      "org.scalamock" %% "scalamock-scalatest-support" % "3.0.1" % "stub->default",
+      "org.scalatest" %% "scalatest" % "2.0.M5b" % "stub->default",
+      "org.scalacheck" %% "scalacheck" % "1.10.1" % "stub->default"))
 
   /*
    * Separated to allow focused development.
@@ -45,11 +74,11 @@ object TreodeBuild extends Build {
 
   lazy val cluster = Project ("cluster", file ("cluster"))
     .dependsOn (pickle)
-    .settings (standardSettings: _*)
+    .settings (withStubSettings: _*)
 
   lazy val store = Project ("store", file ("store"))
-    .dependsOn (pickle, cluster)
-    .settings (standardSettings: _*)
+    .dependsOn (pickle, cluster % "compile;stub->stub")
+    .settings (withStubSettings: _*)
 
   /*
    * Production Server
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin ("com.eed3si9n" % "sbt-assembly" % "0.8.4")
 
-addSbtPlugin ("com.github.mpeltonen" % "sbt-idea" % "1.5.1")
+addSbtPlugin ("com.eed3si9n" % "sbt-assembly" % "0.10.0")
 
+addSbtPlugin ("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.0.0")
