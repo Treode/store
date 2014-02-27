c060098f144e6094ae3392186555235de0e5d964
Topher <topher@treode.com>
2014-02-27 07:20:13 -0800

    Extract buffers into their own project.

--- project/TreodeBuild.scala
+++ project/TreodeBuild.scala
@@ -7,25 +7,17 @@ import sbtassembly.Plugin.assemblySettings
 
 object TreodeBuild extends Build {
 
-  // Adds stub configuration.
-  lazy val Stub = config ("stub") extend (Compile)
-  lazy val CustomTest = config ("test") extend (Stub)
-
-  lazy val commonSettings =
-    inConfig (CustomTest) (Defaults.testSettings) ++
-    Seq (
+  // Settings common to both projects with stubs and without stubs.
+  // Adds production libraries to the "main" configuration.  Squashes
+  // the source directory structure.
+  lazy val commonPortion = Seq (
 
     organization := "com.treode",
     version := "0.1",
     scalaVersion := "2.10.3",
 
-    ivyConfigurations := overrideConfigs (Compile, Stub, CustomTest) (ivyConfigurations.value),
-
     unmanagedSourceDirectories in Compile <<=
       (baseDirectory ((base: File) => Seq (base / "src"))),
-
-    unmanagedSourceDirectories in Stub <<=
-      (baseDirectory ((base: File) => Seq (base / "stub"))),
 
     unmanagedSourceDirectories in CustomTest <<=
       (baseDirectory ((base: File) => Seq (base / "test"))),
@@ -44,9 +36,9 @@ object TreodeBuild extends Build {
       "org.slf4j" % "slf4j-api" % "1.7.5",
       "org.slf4j" % "slf4j-simple" % "1.7.5"))
 
-  lazy val standardSettings =
-    commonSettings ++
-    Seq (
+  // A portion of the settings for projects without stubs.  Adds
+  // testing libraries to SBT's "test" configuration.
+  lazy val standardPortion = Seq (
 
     libraryDependencies ++= Seq (
       "com.github.axel22" %% "scalameter" % "0.3" % "test",
@@ -54,10 +46,24 @@ object TreodeBuild extends Build {
       "org.scalatest" %% "scalatest" % "2.0.M5b" % "test",
       "org.scalacheck" %% "scalacheck" % "1.10.1" % "test"))
 
-  lazy val withStubSettings =
-    inConfig (Stub) (Defaults.configSettings) ++
-    commonSettings ++
-    Seq (
+  // Settings for projects without stubs.
+  lazy val standardSettings =
+    commonPortion ++
+    standardPortion
+
+  lazy val Stub = config ("stub") extend (Compile)
+  lazy val CustomTest = config ("test") extend (Stub)
+
+  // A portion of the settings for projects with stubs.  Adds the
+  // "stub" configuration and creates a replaces the SBT "test"
+  // configuration with a new one that depends on the stubs.  Then
+  // adds the testing libraries to the new "test" configuration.
+  lazy val stubPortion = Seq (
+
+    ivyConfigurations := overrideConfigs (Compile, Stub, CustomTest) (ivyConfigurations.value),
+
+    unmanagedSourceDirectories in Stub <<=
+      (baseDirectory ((base: File) => Seq (base / "stub"))),
 
     EclipseKeys.configurations := Set (Compile, Stub, CustomTest),
 
@@ -67,48 +73,47 @@ object TreodeBuild extends Build {
       "org.scalatest" %% "scalatest" % "2.0.M5b" % "stub->default",
       "org.scalacheck" %% "scalacheck" % "1.10.1" % "stub->default"))
 
-  /*
-   * Separated to allow focused development.
-   */
+  // Settings for projects with stubs.
+  lazy val stubSettings =
+    inConfig (Stub) (Defaults.configSettings) ++
+    inConfig (CustomTest) (Defaults.testSettings) ++
+    commonPortion ++
+    stubPortion
 
-  lazy val pickle = Project ("pickle", file ("pickle"))
+  // Both the async and pickle projects depend on buffers, but someday
+  // someone may want the async package without the picklers or vice
+  // versa, so we have isolated the buffers into their own project.
+  lazy val buffer = Project ("buffer", file ("buffer"))
     .settings (standardSettings: _*)
 
+  // Separated because this may be useful on its own.
   lazy val async = Project ("async", file ("async"))
-    .dependsOn (pickle)
-    .settings (withStubSettings: _*)
+    .dependsOn (buffer)
+    .settings (stubSettings: _*)
 
-  lazy val cluster = Project ("cluster", file ("cluster"))
-    .dependsOn (async % "compile;stub->stub")
-    .settings (withStubSettings: _*)
-
-  lazy val disk = Project ("disk", file ("disk"))
-    .dependsOn (async % "compile;stub->stub")
+  // Separated because this may be useful on its own.
+  lazy val pickle = Project ("pickle", file ("pickle"))
+    .dependsOn (buffer)
     .settings (standardSettings: _*)
 
+  // Separated because it helped development.
+  lazy val cluster = Project ("cluster", file ("cluster"))
+    .dependsOn (async % "compile;stub->stub", pickle)
+    .settings (stubSettings: _*)
+
+  // Separated because it helped development.
+  lazy val disk = Project ("disk", file ("disk"))
+    .dependsOn (async % "compile;test->stub", pickle)
+    .settings (standardSettings: _*)
+
+  // The main component that this repository and build provides.
   lazy val store = Project ("store", file ("store"))
     .dependsOn (cluster % "compile;stub->stub", disk)
-    .settings (withStubSettings: _*)
+    .settings (stubSettings: _*)
 
-  /*
-   * Production Server
-   */
-
-  lazy val treodeSettings =
-    standardSettings ++
-    assemblySettings ++
-  Seq (
-    name := "server",
-    test in assembly := {})
-
-  lazy val server = Project ("server", file ("server"))
-    .dependsOn (store)
-    .settings (treodeSettings: _*)
-
-  /*
-   * System Test, separated to keep test code out of production.
-   */
-
+  // A standalone server for system tests.  Separated to keep system
+  // testing components out of production code (these components are
+  // in the "default" Ivy configuration in this project).
   lazy val systestSettings =
     standardSettings ++
     assemblySettings ++
@@ -117,7 +122,6 @@ object TreodeBuild extends Build {
     test in assembly := {})
 
   lazy val systest = Project ("systest", file ("systest"))
-    .dependsOn (server, store)
+    .dependsOn (store)
     .settings (systestSettings: _*)
-
 }
