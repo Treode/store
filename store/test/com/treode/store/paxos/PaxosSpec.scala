52dc32c51d0b1d4e58918e530ddc48406153adc1
Topher <topher@treode.com>
2014-04-01 14:17:37 -0700

    Switch PaxoSpec from Word/PropSpec to FreeSpec.

--- store/test/com/treode/store/paxos/PaxosSpec.scala
+++ store/test/com/treode/store/paxos/PaxosSpec.scala
@@ -1,6 +1,5 @@
 package com.treode.store.paxos
 
-import java.nio.file.Paths
 import java.util.concurrent.TimeoutException
 import scala.util.Random
 
@@ -8,45 +7,18 @@ import com.treode.async.AsyncChecks
 import com.treode.cluster.StubNetwork
 import com.treode.store.Bytes
 import com.treode.tags.{Intensive, Periodic}
-import org.scalacheck.Gen
-import org.scalatest.{PropSpec, Suites, WordSpec}
+import org.scalatest.FreeSpec
 
 import PaxosTestTools._
 
-class PaxosSpec extends Suites (PaxosBehaviors, new PaxosProperties)
+class PaxosSpec extends FreeSpec with AsyncChecks {
 
-object PaxosBehaviors extends WordSpec {
+  case class Summary (timedout: Boolean, chosen: Set [Int]) {
 
-  private val kit = StubNetwork()
-  private val hs = kit.install (3, new StubPaxosHost (_, kit))
-  private val Seq (h1, h2, h3) = hs
-
-  for (h <- hs)
-    h.setCohorts((h1, h2, h3))
-
-  import kit.{random, scheduler}
-  import h1.acceptors
-  import h1.paxos.lead
-
-  kit.runTasks()
-
-  "The paxos implementation" should {
-
-    val k = Bytes (random.nextLong)
-
-    "yield a value for the leader" in {
-      lead (k, 1) .expect (1)
-    }
-
-    "leave all acceptors closed and consistent" in {
-      val as = hs map (_.acceptors.get (k))
-      assert (as forall (_.isClosed))
-      assertResult (Set (1)) (as.map (_.getChosen) .flatten.toSet)
-    }}}
-
-class PaxosProperties extends PropSpec with AsyncChecks {
-
-  case class Summary (timedout: Boolean, chosen: Set [Int])
+    def check (domain: Set [Int]) {
+      if (intensity == "standard")
+        assertResult (domain) (chosen)
+    }}
 
   def checkConsensus (random: Random, mf: Double, summary: Summary): Summary = {
     val kit = StubNetwork (random)
@@ -54,7 +26,7 @@ class PaxosProperties extends PropSpec with AsyncChecks {
     val Seq (h1, h2, h3) = hs
 
     for (h <- hs)
-    h.setCohorts((h1, h2, h3))
+      h.setCohorts((h1, h2, h3))
 
     import kit.scheduler
 
@@ -62,7 +34,7 @@ class PaxosProperties extends PropSpec with AsyncChecks {
 
       val k = Bytes (random.nextLong)
 
-      // Proposed two values simultaneously, expect one choice.
+      // Propose two values simultaneously, expect one choice.
       val cb1 = h1.paxos.propose (k, 1) .capture()
       val cb2 = h2.paxos.propose (k, 2) .capture()
       kit.messageFlakiness = mf
@@ -81,20 +53,22 @@ class PaxosProperties extends PropSpec with AsyncChecks {
         Summary (true, summary.chosen)
     }}
 
-  property ("The acceptors should achieve consensus", Intensive, Periodic) {
-    var summary = Summary (false, Set.empty)
-    forAllSeeds { random =>
-      summary = checkConsensus (random, 0.0, summary)
-    }
-    if (intensity == "standard")
-      assert (Seq (1, 2) forall (summary.chosen contains _))
-  }
+  "The acceptors should" - {
 
-  property ("The acceptors should achieve consensus with a flakey network", Intensive, Periodic) {
-    var summary = Summary (false, Set.empty)
-    forAllSeeds { random =>
-      summary = checkConsensus (random, 0.1, summary)
-    }
-    if (intensity == "standard")
-      assert (Seq (1, 2) forall (summary.chosen contains _))
-  }}
+    "achieve consensus with" - {
+
+      "stable hosts and a reliable network" taggedAs (Intensive, Periodic) in {
+        var summary = Summary (false, Set.empty)
+        forAllSeeds { random =>
+          summary = checkConsensus (random, 0.0, summary)
+        }
+        summary.check (Set (1, 2))
+      }
+
+      "stable hosts and a flakey network" taggedAs (Intensive, Periodic) in {
+        var summary = Summary (false, Set.empty)
+        forAllSeeds { random =>
+          summary = checkConsensus (random, 0.1, summary)
+        }
+        summary.check (Set (1, 2))
+      }}}}
