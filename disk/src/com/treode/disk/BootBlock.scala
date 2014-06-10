c3b346fbe7066aa212094356ca98e708f4c7b0fa
Topher <topher@treode.com>
2014-06-10 09:44:31 -0700

    BootBlock's hashCode and equals must account for sysid being a byte array.

--- disk/src/com/treode/disk/BootBlock.scala
+++ disk/src/com/treode/disk/BootBlock.scala
@@ -1,12 +1,27 @@
 package com.treode.disk
 
+import java.lang.{Integer => JInt}
 import java.nio.file.Path
+import java.util.{Arrays, Objects}
 
 private case class BootBlock (
     sysid: Array [Byte],
     gen: Int,
     number: Int,
-    disks: Set [Path])
+    disks: Set [Path]) {
+
+  override def hashCode: Int =
+    Objects.hash (Arrays.hashCode (sysid): JInt, gen: JInt, number: JInt, disks)
+
+  override def equals (other: Any): Boolean =
+    other match {
+      case that: BootBlock =>
+        Arrays.equals (sysid, that.sysid) &&
+        gen == that.gen &&
+        number == that.number &&
+        disks == that.disks
+      case _ => false
+    }}
 
 private object BootBlock {
 
