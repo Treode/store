ef9188e3453b01cf2b1765983dca2929ccf74c1a
Topher <topher@treode.com>
2014-06-11 09:35:54 -0700

    Add toString to Backoff.

--- async/src/com/treode/async/Backoff.scala
+++ async/src/com/treode/async/Backoff.scala
@@ -18,18 +18,21 @@ import Backoff.BackoffIterator
   * and you can use [[com.treode.async.stubs.AsyncChecks AsyncChecks]] to seed a random in testing.
   */
 class Backoff private (
-  val start: Int,
-  val jitter: Int,
-  val max: Int,
-  val retries: Int
+    start: Int,
+    jitter: Int,
+    max: Int,
+    retries: Int
 ) {
 
   require (start > 0 || jitter > 0, "Start or jitter must be greater than 0.")
-  require (max > start + jitter, s"Max must be greater than start + jitter $max $start $jitter")
+  require (max > start + jitter, "Max must be greater than start + jitter")
   require (retries >= 0, "Retries must be non-negative")
 
   def iterator (implicit random: Random): Iterator [Int] =
     new BackoffIterator (random, max, start + random.nextInt (jitter), retries)
+
+  override def toString: String =
+    s"Backoff($start, $jitter, $max, $retries)"
 }
 
 object Backoff {
