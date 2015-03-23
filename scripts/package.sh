7a736eafac9e1fe24c2cc38d32eb7a821916a870
BrainFlame <BrainFlame@users.noreply.github.com>
2015-03-22 18:02:41 -0700

    Integration with build script. Cleanup and indentation. Allow version number to be passed from build-merge build script.

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,5 @@
 #/bin/bash -e
-# Copyright 2014 Treode, Inc.
+# Copyright 2015 Treode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,45 +13,99 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SCRIPT_DIR=$(dirname $(readlink -f $0))
+create_debian_package () {
+    $PKGVER=$1
+    $WGETFILE=$2
+    $JARFILE=$3
+    $TREODE_STAGEDIR=$4
 
-echo "PACKAGING TREODE FOR Debian..."
+    STAGEDIR0=/var/tmp/treode.$$
+    STAGEDIR=${STAGEDIR0}/treode-${PKGVER}
+    mkdir -p ${STAGEDIR}
 
-STAGEDIR0=/var/tmp/treode.$$
-STAGEDIR=${STAGEDIR0}/treode-0.2.0
-mkdir -p ${STAGEDIR}
+    cp ${SCRIPT_DIR}/conf/treode.init ${STAGEDIR}
+    cd ${STAGEDIR}
 
-cp ${SCRIPT_DIR}/conf/treode.init ${STAGEDIR}
-cd ${STAGEDIR}
+    if [[ $WGETFILE == 1 ]]; then
+        wget https://oss.treode.com/examples/finagle/0.2.0/finagle-server-0.2.0.jar
+    else
+	echo COPY $JARFILE to `pwd`
+        cp $JARFILE .
+    fi
 
-wget https://oss.treode.com/examples/finagle/0.2.0/finagle-server-0.2.0.jar
+    LOGNAME=questions@treode.com
+    DEBFULLNAME="Treode Inc"
+    dh_make -y -n -s -c apache -e ${LOGNAME}
 
-LOGNAME=treode@treode.com
-DEBFULLNAME="Treode Inc"
-dh_make -y -n -s -c apache -e treode@treode.com
-
-# Customize debian control files
-cat ${SCRIPT_DIR}/conf/treode.rules >> debian/rules
-sed -i -e '/^Depends:/ s/$/, \${treode:Depends}/' debian/control
-sed -i -e 's/^Homepage:/Homepage: http:\/\/www.treode.com/' debian/control
-sed -i -e '/^Description:/{N;s/.*/Description: The DB that\x27s replicated, sharded and transactional.\
+    # Customize debian control files
+    cat ${SCRIPT_DIR}/conf/treode.rules >> debian/rules
+    sed -i -e '/^Depends:/ s/$/, \${treode:Depends}/' debian/control
+    sed -i -e 's/^Homepage:/Homepage: http:\/\/www.treode.com/' debian/control
+    sed -i -e '/^Description:/{N;s/.*/Description: The DB that\x27s replicated, sharded and transactional.\
  TreodeDB is an open-source NoSQL database that shards for scalability, replicates for \
  reliability, and yet provides full ACID transactions.\
  TreodeDB connects to Spark for analytics, and it integrates well with CDNs for speed.\
  TreodeDB lets engineers develop the application, rather than work around the data architecture./}' debian/control
 
+    export DESTDIR=`pwd`/debian
+    fakeroot debian/rules clean
+    PKGVER="${PKGVER}" fakeroot debian/rules binary
 
-export DESTDIR=`pwd`/debian
-fakeroot debian/rules clean
-fakeroot debian/rules binary
+    # If successful, copy .deb to $SCRIPT_DIR/o
+    if [ -e ${STAGEDIR}/../*.deb ]; then
+        mv ${STAGEDIR}/../*.deb ${TREODE_STAGEDIR}
+        rm -rf ${STAGEDIR}
+    else
+        echo "THERE WAS AN ERROR CREATING .DEB PACKAGE"
+        echo "STAGE DIR ${STAGEDIR}"
+    fi
 
-# If successful, copy .deb to $SCRIPT_DIR/o
-if [ -e ${STAGEDIR}/../*.deb ]; then
-   mkdir -p ${SCRIPT_DIR}/o
-   mv ${STAGEDIR}/../*.deb ${SCRIPT_DIR}/o
-   rm -rf ${STAGEDIR}
-else
-   echo "THERE WAS AN ERROR CREATING .DEB PACKAGE"
-   echo "STAGE DIR ${STAGEDIR}"
+}
+
+PKGTYPE=
+PKGVER=
+JARFILE=
+TREODE_STAGEDIR=
+WGETFILE=0
+while getopts "t:V:j:o:" OPTION
+do
+    case $OPTION in
+        t)
+            PKGTYPE=$OPTARG
+            ;;
+        V)
+            PKGVER=$OPTARG
+            ;;
+        j)
+            JARFILE=`readlink -f $OPTARG`
+            ;;
+        o)
+            TREODE_STAGEDIR=`readlink -f $OPTARG`
+            ;;
+    esac
+done
+
+if [[ -z $PKGTYPE ]] || [[ -z $PKGVER ]] || [[ -z $TREODE_STAGEDIR ]] ; then
+    echo "Please provide package type and version using -t and -V options"
+    exit 1
 fi
+
+if [[ -z $JARFILE ]] ; then
+    WGETFILE=1
+    JARFILE="https://oss.treode.com/examples/finagle/${PKGVER}/finagle-server-${PKGVER}.jar"
+    echo "Using $JARFILE"
+fi
+
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+
+case $PKGTYPE in
+    debian)
+        echo "PACKAGING TREODE FOR Debian..."
+        create_debian_package $PKGVER $WGETFILE $JARFILE $TREODE_STAGEDIR
+        ;;
+    ?)
+        echo "PACKAGING TREODE FOR $PKGTYPE is not implemented"
+        exit 1
+        ;;
+esac
 
