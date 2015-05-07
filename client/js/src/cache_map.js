ba59415cab56dc1f83d4d59f9082ff211484337a
Mike <mike@toms-mbp.att.net>
2015-05-06 19:22:47 -0500

    Add LRU cache and associated tests for JS.
    closes #155

---
+++ client/js/src/cache_map.js
@@ -0,0 +1,284 @@
+/*
+ * Copyright 2014 Treode, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var tx_clock = require('./tx_clock');
+
+/** Constructor */
+function cache_map(cache_limit) {
+  this._size  = 0;
+  this._limit = cache_limit;
+  this._map   = {};
+  this._head = null;
+  this._tail = null;
+}
+
+cache_map.prototype = {
+  constructor: cache_map,
+
+  /** Adds an object to the head of our list.
+    * @private
+    */
+  _add: function(node)
+  {
+    if(this._size == 0) //if this is the only item
+    {
+      this._head = node;
+      this._tail = node;
+      return 1;
+    }
+    this._head.next = node;
+    node.previous = this._head;
+    this._head = node;
+    return 1;
+  },
+
+  /** Based on whether there exists a list for this key:value, either a new list is added or we
+    * append a new list object to an existing list.
+    * @public
+    */
+  put: function(read_time, value_time, table, key, value)
+  {
+    if(read_time.constructor == Number)
+      read_time = new tx_clock(read_time);
+    if(value_time.constructor == Number)
+      value_time = new tx_clock(value_time);
+
+    var k     = this._keyGen(key, table);
+    var array = this._map[k];
+    if(array === undefined)         //there exists no entry for that key
+      this._addList(read_time, value_time, table, key, value);
+    else
+      this._appendList(read_time, value_time, table, key, value);
+  },
+
+  /** Create new entry in map. Will generate new object entry and insert into list, then place
+    * reference to list entry into a new list in the object map.
+    * @private
+    */
+  _addList: function(read_time, value_time, table, key, value)
+  {
+    //create new entry in map
+    var k = this._keyGen(key, table);
+
+    //create new entry in list
+    var list_entry = {
+      key:key,
+      table:table,
+      value_time:value_time,
+      cached_time:read_time,
+      value:value,
+      next:null,
+      previous:null
+    };
+
+    this._add(list_entry);
+    this._map[k] = [this._head];    //insert reference to list object
+    this._size++;
+    this._prune();
+    return;
+  },
+
+  /** Appends to an existing list for a specific key. Checks for an element in our list which
+    * contains a value_time equal to the value_time we are inserting. If so, update the cached_time
+    * to the later of the element's cached_time and the read_time parameter. If updating, promote
+    * list element to mru, otherwise create a new entry
+    * @private
+    */
+  _appendList: function(read_time, value_time, table, key, value)
+  {
+    var k = this._keyGen(key, table);
+    for(var i in this._map[k]) //for each key:table
+    {
+      if(+this._map[k][i].value_time == +value_time)
+      {
+        var optimal = new tx_clock(
+          Math.max(this._map[k][i].cached_time, read_time)
+          );
+        this._map[k][i].cached_time = optimal;
+        this._promote(this._map[k][i]); //mru, so promote it
+        return 1;
+      }
+    }
+
+    var list_entry = {
+      key:key,
+      table:table,
+      value_time:value_time,
+      cached_time:read_time,
+      value:value,
+      next:null, //add will set next and prev pointers
+      previous:null
+    };
+    this._add(list_entry);
+    this._map[k].push(this._head);
+    this._size++;
+    this._prune();
+    return 1;
+  },
+
+  /** Remove the lru object from list and map.
+    * @private
+    */
+  _prune: function()
+  {
+    while(this._size > this._limit)
+      this._evictOne();
+  },
+
+  /** Remove the lru element from the list and then remove the empty entry from the map. If removal
+    * of the map element results in an empty list then we completely remove the entry in the map.
+    * In this implementation of the linked list, the lru entry is at the head of the list.
+    * @private
+    */
+  _evictOne: function()
+  {
+    var lru     = this._removeTail();
+    var key     = this._keyGen(lru.key,lru.table);
+    var array   = this._map[key];
+
+    for(var i in this._map[key])
+    {
+      if(+this._map[key][i].value_time == +lru.value_time)
+      {
+        this._map[key].splice(i, 1); //will remove 1 cell from array
+        this._size--;
+      }
+    }
+
+    //check for an empty list length and remove key entirely if empty
+    if(this._map[key].length == 0)
+    {
+      delete this._map[key];
+    }
+  },
+
+  /** Dissociates the tail from dll and returns it to caller.
+    * @private
+    */
+  _removeTail: function()
+  {
+    if(this._tail == null)
+      return null;
+    var removed = this._tail;
+    if(this._size > 1) //where the tail != head
+    {
+      removed.next.previous = null;
+      this._tail = removed.next;
+    }
+    else //where the tail == head
+    {
+      this._tail = null;
+      this._head = null;
+    }
+    return removed;
+  },
+
+
+  /** Retrives a tuple of value_time, cached_time, and value based on the time we are reading for a
+    * given object associated with the given table and key. Finding a specific element requires
+    * finding an element whose value was written sometime before we are reading, from that subset
+    * of elements we choose the maximum value_time. Finally, the element that we find gets promoted.
+    * @public
+    */
+  get: function(read_time, table, key)
+  {
+
+    //1)search map for table:key and the most recent value_time on or before the given read_time
+    //2)promote the use time in this.list
+
+    //convert read_time param to a tx_clock
+    if(read_time.constructor == Number)
+      read_time = new tx_clock(read_time);
+
+    var k = this._keyGen(key, table);
+    var array = this._map[k];
+
+    //check for a completely invalid key, table pair
+    if(array == undefined || array.length == 0)
+      return -1;
+
+    var max         = undefined; //maybe we won't find anything
+    var found_first = false; //select the first plausible object
+    for(var i in this._map[k])
+    {
+      var vt = this._map[k][i].value_time
+      if(+vt <= +read_time && !found_first) //so we can set the max to the first eligible val.
+      {
+        max = this._map[k][i];
+        found_first = true;
+        continue;
+      }
+      if(found_first)  //so we don't worry about reading an invalid 'max'
+      {
+        if(+vt <= +read_time && vt > max.value_time)
+          max = this._map[k][i];
+      }
+    }
+    if(max == undefined)
+      return -1; //couldn't find in the cache
+
+    this._promote(max);
+
+    return {
+      value_time:max.value_time,
+      cached_time:max.cached_time,
+      value:max.value
+    };
+  },
+
+  /** Generates a single string for a given key and table. This format is used to read and write to
+    * the internal map.
+    * @private
+    */
+  _keyGen:function(key, table)
+  {
+    return key + ":" + table;
+  },
+
+  /** Promotes a given list element to the front of our dll. In this implementation of the dll, the
+    * mru object is at the tail of the list, and the lru is at the head of the list. If the node is
+    * at the tail then its already the mru. If node is at the head then we set pointers to create a
+    * new head. If node is neither head nor tail then rearrange pointers to _evict element from cur
+    * position and insert at tail.
+    * @private
+    */
+  _promote:function(node)
+  {
+    var isHead = this._head == node;
+    var isTail = this._tail == node;
+    if(node !== null)
+    {
+      if(isHead)
+        return; //we are already at the mru position
+      if(isTail)
+      {
+        node.next.previous = null; //cut off from list
+        this._tail = node.next; //set tail ptr
+        this._head.next = node; //add to front
+        node.previous = this._head; //link backward to rest of list
+        this._head = node; //set head ptr
+        return;
+      }
+      node.next.previous = node.previous; //removes references to this
+      node.previous.next = node.next;
+      this._head.next = node; //point it to front
+      node.previous = this._head; //link backward to rest of list
+      this._head = node; //set head ptr
+    }
+  }
+}
+
+module.exports = cache_map;
---
+++ client/js/test/cache_map_test.js
@@ -0,0 +1,372 @@
+/*
+ * Copyright 2014 Treode, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var cache_map = require('../src/cache_map.js');
+var test = require('unit.js');
+
+//cache_map put tests
+describe('cache_map.put()',function(){
+
+	//most basic test, should accept 3 puts and all elements should be in the cache map
+	//does not validate values, that is done in 'get' tests
+	it('should place 3 objects with different keys in the cache ', function(){
+		var cache_size = 3;
+		var pass_condition = false;
+		var c = new cache_map(cache_size);
+
+		c.put(1,2,"fruit","apple","red");
+		c.put(3,4,"fruit","banana","yellow");
+		c.put(5,6,"sport","tennis","racquet");
+
+		//check that each item gets its own list in the cache
+		var count = 0;
+		for(var i in c._map)
+			count++;
+		//make sure we counted correct number of entries
+
+		test.value(count).isEqualTo(cache_size);
+		test.value(c._size).isEqualTo(cache_size);
+	});
+
+	//maps same key:table with varying value_times and values,
+	//there should be 3 distinct elements in a single list entry
+
+	it('should place 3 objects with same keys and different values in the cache ', function(){
+		var cache_size = 3;
+		var pass_condition = false;
+		var c = new cache_map(cache_size);
+
+		c.put(1,2,"key","table","val_1");
+		c.put(3,4,"key","table","val_2");
+		c.put(5,6,"key","table","val_3");
+
+		var count_keys = 0;
+		var count_len  = 0;
+		for(var i in c._map)			//for keys in map
+		{
+			count_keys++;
+			for(var x in c._map[i])	//for entries in map[key]'s list
+				count_len++;
+		}
+
+		test.value(count_keys).isEqualTo(1);
+		test.value(count_len).isEqualTo(cache_size);
+	});
+
+	//cache should run out of space and evict the first 3 items to make room for new puts
+
+	it('should evict the first three items placed in cache p1', function(){
+		var cache_size = 3;
+		var pass_condition_1 = true;
+		var pass_condition_2 = true;
+		var c = new cache_map(cache_size);
+
+		c.put(1,2,"fruit","apple",    "a");
+		c.put(3,4,"fruit","banana",   "b");
+		c.put(5,6,"sport","tennis",   "c");
+		c.put(7,8,"furniture","chair","1");
+		c.put(9,0,"ascii","letter",   "2");
+		c.put(1,2,"book","biography", "3");
+
+
+		var count_keys = 0;
+		var count_elements = 0;
+		for(var key in c._map)
+		{
+			count_keys++;
+			var arr = c._map[key];
+			for(var i in c._map[key])
+			{
+				count_elements++;
+				var v = arr[i].value;
+
+				var equal = (v == "1") || (v == "2") || (v =="3");	//check for valid
+				test.value(equal).isEqualTo(true);
+
+				var notEqual = (v != "a" && v != "b" && v != "c");
+				test.value(notEqual).isEqualTo(true);				//check for invalid
+
+			}
+		}
+
+		test.value(count_elements).isEqualTo(cache_size);
+		test.value(count_keys).isEqualTo(cache_size);
+	});
+
+	//similar test except this checks if eviction works when there exists only 1 key for multiple values
+
+	it('should evict the first three items placed in cache p2', function(){
+		var cache_size = 3;
+		var pass_condition_1 = true;
+		var pass_condition_2 = true;
+		var c = new cache_map(cache_size);
+
+		c.put(1,1,"table","key","a");
+		c.put(2,2,"table","key","b");
+		c.put(3,3,"table","key","c");
+		c.put(4,4,"table","key","1");
+		c.put(5,5,"table","key","2");
+		c.put(6,6,"table","key","3");
+
+
+		var count_keys = 0;
+		for(var key in c._map)
+		{
+			var arr = c._map[key];
+			for(var i in c._map[key])
+			{
+				count_keys++;
+				var v = arr[i].value;
+
+				var equal = (v == "1") || (v == "2") || (v =="3");	//check for valid
+				test.value(equal).isEqualTo(true);
+
+				var notEqual = (v != "a" && v != "b" && v != "c");
+				test.value(notEqual).isEqualTo(true);				//check for invalid
+
+			}
+		}
+
+		test.value(count_keys).isEqualTo(cache_size);
+	});
+
+	//tests that updating the cached_time of an object makes it mru and doesn't add new entry
+
+	it('should evict the first 2 items placed in cache and update cache_time p1', function(){
+		var cache_size = 3;
+		var pass_condition_1 = true;
+		var pass_condition_2 = true;
+		var c = new cache_map(cache_size);
+
+		c.put(1,2,"fruit","apple",    "a");
+		c.put(3,4,"fruit","banana",   "b");
+		c.put(5,6,"sport","tennis",   "1");
+		c.put(7,8,"furniture","chair","2");
+		c.put(6,6,"sport","tennis",   "1");			//picking a value_time equivalent to 3rd element, should be mru
+		c.put(1,2,"book","biography", "3");
+
+
+		var count_keys = 0;
+		for(var key in c._map)
+		{
+			var arr = c._map[key];
+			for(var i in c._map[key])
+			{
+				count_keys++;
+				var v = arr[i].value;
+
+				var equal = (v == "1") || (v == "2") || (v == "3");
+				var notEqual = (v != "a") && (v != "b");
+
+				test.value(equal).isEqualTo(true);
+				test.value(notEqual).isEqualTo(true);
+			}
+		}
+		test.value(count_keys).isEqualTo(cache_size);
+	});
+
+	//tests that updating works similarly when we have multiple elements in the map[key] list
+
+	it('should evict 2 items from cache and update cache_time for the first element', function(){
+		var cache_size = 3;
+		var pass_condition_1 = true;
+		var pass_condition_2 = true;
+		var c = new cache_map(cache_size);
+
+		c.put(1,1,"table","key","a");
+		c.put(2,2,"table","key","b");
+		c.put(3,3,"table","key","c");
+		c.put(2,1,"table","key","a");	//should update first put
+		c.put(5,5,"table","key","1");
+		c.put(6,6,"table","key","2");
+
+
+		var count_keys = 0;
+		for(var key in c._map)
+		{
+			var arr = c._map[key];
+			for(var i in c._map[key])
+			{
+				count_keys++;
+				var v = arr[i].value;
+
+				var equal = (v == "1") || (v == "2") || (v == "a");
+				var notEqual = (v != "c") && (v != "b");
+
+				test.value(equal).isEqualTo(true);
+				test.value(notEqual).isEqualTo(true);
+
+			}
+		}
+		test.value(count_keys).isEqualTo(cache_size);
+
+	});
+
+});
+
+//Tests related to checking correct values for a given key, table, and readtime
+
+describe('cache_map.get()',function(){
+
+	//basic test to ensure all elements made it into the cache
+
+	it('should retrive all elements in cache p1', function(){
+		var late_time = 100;
+		var cache_size = 3;
+		var c = new cache_map(cache_size);
+		var pass_condition = true;
+
+		c.put(1,2,"fruit","apple", "a");
+		c.put(3,4,"fruit","banana","b");
+		c.put(5,6,"sport","tennis","c");
+
+		var appleGet = c.get(late_time,"fruit","apple").value;
+		var bananaGet = c.get(late_time,"fruit","banana").value;
+		var tennisGet = c.get(late_time,"sport","tennis").value;
+		test.value(appleGet).isEqualTo("a");
+		test.value(bananaGet).isEqualTo("b");
+		test.value(tennisGet).isEqualTo("c");
+	});
+
+	//similar test but instead mapping elements to the same key with different value_times
+
+	it('should retrive all elements in cache p2', function(){
+		var late_time = 100;
+		var cache_size = 3;
+		var c = new cache_map(cache_size);
+		var pass_condition = true;
+
+		c.put(1,1,"table","key","a");
+		c.put(2,2,"table","key","b");
+		c.put(3,3,"table","key","c");
+
+		var getEarly = c.get(1,"table","key").value;
+		var getMid = c.get(2,"table","key").value;
+		var getLate = c.get(3,"table","key").value;
+
+		test.value(getEarly).isEqualTo("a");
+		test.value(getMid).isEqualTo("b");
+		test.value(getLate).isEqualTo("c");
+	});
+
+	//check if reading at earlier time prevents reading values written at a later time
+
+	it('should miss certain elements due to early read times p1', function(){
+		var late_time  = 100;
+		var early_time = 1;
+		var cache_size = 3;
+		var c = new cache_map(cache_size);
+		var pass_condition = true;
+
+		c.put(10,20,"fruit","apple", "a");
+		c.put(30,40,"fruit","banana","b");
+		c.put(50,60,"sport","tennis","c");
+
+		var getApple = c.get(late_time,"fruit","apple").value;
+		var getBananaEarly = c.get(early_time,"fruit","banana");
+		var getTennisEarly = c.get(early_time,"sport","tennis");
+		var getBananaLate = c.get(late_time,"fruit","banana").value;
+		var getTennisLate = c.get(late_time,"sport","tennis").value;
+
+		test.value(getApple).isEqualTo("a");
+		test.value(getBananaLate).isEqualTo("b");
+		test.value(getTennisLate).isEqualTo("c");
+		test.value(getTennisEarly).isEqualTo(-1);
+		test.value(getBananaEarly).isEqualTo(-1);
+
+	});
+
+	it('should miss certain elements due to early read times p2', function(){
+		var late_time = 100;
+		var med_time = 1;
+		var early_time = 0;
+		var cache_size = 3;
+		var c = new cache_map(cache_size);
+		var pass_condition = true;
+
+		c.put(3,3,"table","key","a");
+		c.put(2,2,"table","key","b");
+		c.put(1,1,"table","key","c");
+
+		var late = c.get(late_time,"table","key").value;
+		var mid = c.get(med_time,"table","key").value;
+		var early = c.get(early_time,"table","key");
+
+		test.value(late).isEqualTo("a");
+		test.value(mid).isEqualTo("c");
+		test.value(early).isEqualTo(-1);
+	});
+
+	//should ensure getting an element promotes that element to mru
+	//also checks that invalid 'get' doesn't mistakenly update another value
+	it('should promote a get request to most recently used', function(){
+		var late_time  = 100;
+		var early_time = 1
+		var cache_size = 3;
+		var c = new cache_map(cache_size);
+		var pass_condition = true;
+
+		c.put(10,20,"fruit","apple", "a");
+		c.put(30,40,"fruit","banana","b");
+		c.put(50,60,"sport","tennis","c");
+		c.get(late_time, "fruit", "apple");			//will update first element that was entered
+		c.put(70,80,"device","phone", "cell");
+		c.get(early_time,"fruit","banana");			//will not update to mru
+		c.put(80,90,"U.S","Chicago","park");
+
+		var getTennis = c.get(late_time,"sport","tennis");
+		var getApple = c.get(late_time,"fruit","apple").value;
+		var getPhone = c.get(late_time, "device","phone").value;
+		var getChicago = c.get(late_time, "U.S", "Chicago").value;
+		var getBanana = c.get(late_time,"fruit","banana");
+
+		test.value(getTennis).isEqualTo(-1);
+		test.value(getApple).isEqualTo("a");
+		test.value(getPhone).isEqualTo("cell");
+		test.value(getChicago).isEqualTo("park");
+		test.value(getBanana).isEqualTo(-1);
+	});
+
+	//should ensure we dont have access to things outside of cachelimits
+
+	it('should miss on entries outside of cache and non existent entries', function(){
+
+		var late_time = 100;
+		var early_time = 1;
+		var cache_size = 3;
+		var c  = new cache_map(cache_size);
+		var pass_condition = true;
+
+		c.put(10,10,"k","t","v0");
+		c.put(11,11,"k","t","v1");
+		c.put(12,12,"k","t","v2");
+		c.get(10,"k","t");
+		c.put(14,14,"fruit","apple","red");
+
+		var getEarly = c.get(9,"k","t");
+		var getLate = c.get(15,"fruit","apple").value;
+		var getMidOne = c.get(10,"k","t").value;
+		var getMidTwo = c.get(11,"k","t").value;
+		var getInvalid = c.get(late_time,"key","table");
+
+		test.value(getEarly).isEqualTo(-1);
+		test.value(getLate).isEqualTo("red");
+		test.value(getMidOne).isEqualTo("v0");
+		test.value(getMidTwo).isEqualTo("v0");
+		test.value(getInvalid).isEqualTo(-1);
+	});
+
+});
